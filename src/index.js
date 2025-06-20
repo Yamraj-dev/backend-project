bfae994830fbf1e421c50d8acc2299aad66f044c
Prathamesh-dev <prathameshmonde.pm@gmail.com>
2025-06-20 20:01:29 +0530

    add utils

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import dotenv from "dotenv";
 dotenv.config({path: "./"});
 import express from "express";
 import connectDB from "./db/db.js";
+import app from "./app.js";
+ 
+app.on("error", (error) => {
+    console.error("error: ", error);
+    throw error;
+})
 
-connectDB();
+connectDB()
+.then(()=> {
+    app.listen(process.env.PORT, () => {
+        console.log("server is running ar ", process.env.PORT)
+    });
+})
+.catch((err)=> {
+    console.error("Mongo connection failed ! " ,err);
+})
