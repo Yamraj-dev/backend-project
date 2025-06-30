6a3ec375af98fe12e0f3851e21bc72ddcb24716f
Yamraj-Dev <yamrajgaming007@gmail.com>
2025-06-30 19:47:25 +0530

    refactor: update refreshAccessToken controller and add route for token refresh

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -147,20 +147,42 @@ export const logOutUser = asyncHandler(async (req, res) => {
     .json( new ApiResponse(200, {}, "User logged out") )
 })
 
-const refreshAccessToken = asyncHandler( async (req, res) => {
+export const refreshAccessToken = asyncHandler( async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshTokene || req.body.refreshToken;
 
     if (!incomingRefreshToken) {
         throw new ApiError(401, "unathorized request");
     }
 
-    const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
-
-    const user = await User.findById(decodedToken?._id);
-    if (!user) {
-        throw new ApiError(401, "invalid refresh token");
-    }
-
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
     
+        const user = await User.findById(decodedToken?._id);
+        if (!user) {
+            throw new ApiError(401, "Invalid refresh token");
+        }
+    
+        if (user.refreshToken !== incomingRefreshToken) {
+            throw new ApiError(401, "Refresh token is expired or invalid");
+        }
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+        const {accessToken, newRefreshToken} = await genrateAccessAndRefereshTokens(user._id);
+    
+        return res.status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookieI("refreshToken", newRefreshToken, options)
+        .json( new ApiResponse(
+            200, 
+            { accessToken, newRefreshToken}, 
+            "Axxxess token refreshed successfully"
+        ))
+    } catch (error) {
+        throw new ApiError(401, "Invalid refresh token");
+    }
 
 })
