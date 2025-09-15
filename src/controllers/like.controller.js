1b068e5da5517e2cf446109e87c059da733cbe65
Prathamesh-dev <prathameshmonde.pm@gmail.com>
2025-09-15 13:45:48 +0530

    add like functionality for tweets, comments, and videos with retrieval of likes by user fix some typos in routes

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,96 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import { Like } from "../models/like.model.js";
+
+export const likeTweet = asyncHandler(async (req, res) => {
+    const { id: tweetId } = req.params;
+
+    const existingLike = await Like.findOne({
+        tweet: tweetId,
+        likedBy: req.user._id
+    });
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id);
+        return res
+            .status(200)
+            .json(new ApiResponse(200, null, "Unliked the tweet"));
+    }
+
+    const like = await Like.create({
+        tweet: tweetId,
+        likedBy: req.user._id
+    });
+
+    res
+        .status(200)
+        .json(new ApiResponse(200, like, "Liked the tweet"));
+});
+
+
+
+export const likeComment = asyncHandler(async (req, res) => {
+    const { id: commentId } = req.params;
+
+    const existingLike = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user._id
+    });
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id);
+        return res
+            .status(200)
+            .json(new ApiResponse(200, null, "Unliked the comment"));
+    }
+
+    const like = await Like.create({
+        comment: commentId,
+        likedBy: req.user._id
+    });
+
+    res
+        .status(200)
+        .json(new ApiResponse(200, like, "Liked the comment"));
+});
+
+
+export const likeVideo = asyncHandler(async (req, res) => {
+    const { id: videoId } = req.params;
+
+    const existingLike = await Like.findOne({
+        video: videoId,
+        likedBy: req.user._id
+    });
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id);
+        return res
+            .status(200)
+            .json(new ApiResponse(200, null, "Unliked the video"));
+    }
+
+    const like = await Like.create({
+        video: videoId,
+        likedBy: req.user._id
+    });
+
+    res
+        .status(200)
+        .json(new ApiResponse(200, like, "Liked the video"));
+});
+
+
+export const likedByUser = asyncHandler(async (req, res) => {
+    const { id: userId } = req.params;
+
+    const likes = await Like.find({ likedBy: userId })
+        .populate("comment")
+        .populate("tweet")
+        .populate("video");
+
+    res
+        .status(200)
+        .json(new ApiResponse(200, likes, "All likes by user fetched"));
+});
--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -1,39 +1,52 @@
 import { Router } from "express";
-import { upload } from "../middlewares/multer.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js"; 
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { deleteVideo, getAllVideos, getUserVideos, getVideoById, updateVideo, uploadVideo } from "../controllers/video.controller.js";
+import {
+    deleteVideo,
+    getAllVideos,
+    getUserVideos,
+    getVideoById,
+    updateVideo,      
+    uploadVideo
+} from "../controllers/video.controller.js";
+
 
 const router = Router();
+
 router.use(verifyJwt);
 
-//secured routes
-router.post("/upload", upload.fields([
-    {
-        name: "videoFile",
-        maxCount: 1
-    },
-    {
-        name: "thumbnailImg",
-        maxCount: 1
-    }
-]), uploadVideo);
-router.post("/getById/:id", getVideoById);
-router.post("/getUser/:id", getUserVideos);
+//secure routes
 
-router.get("/getAll", getAllVideos);
+router.post(
+    "/videos",
+    upload.fields([{ name: "videoFile", maxCount: 1 }, { name: "thumbnailImg", maxCount: 1 }]),
+    uploadVideo
+);
 
-router.put("/update/:id", upload.fields([
-    {
-        name: "videoFile",
-        maxCount: 1
-    },
-    {
-        name: "thumbnailImg",
-        maxCount: 1
-    }
-]), updateVideo);
+router.get(
+    "/videos",
+    getAllVideos
+);
 
-router.delete("/delete/:id", deleteVideo);
+router.get(
+    "/videos/:id",
+    getVideoById
+);
 
+router.get(
+    "/videos/:Id",
+    getUserVideos
+);
+
+router.patch(
+    "/videos/:id",
+    upload.fields([{ name: "videoFile", maxCount: 1 }, { name: "thumbnailImg", maxCount: 1 }]),
+    updateVideo
+);
+
+router.delete(
+    "/videos/:id",
+    deleteVideo
+);
 
 export default router;
