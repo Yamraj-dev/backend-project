fad1d3f4838859a4213490e1b33ddf0499e8822a
Prathamesh-dev <prathameshmonde.pm@gmail.com>
2025-09-16 10:56:52 +0530

    fix typo in like route

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -12,15 +12,15 @@ const router = Router();
 router.use(verifyJwt); 
 
 // comment like/unlike
-router.post("/comments/:id/like", likeComment);
+router.post("/comment/:id/like", likeComment);
 
 // tweet like/unlike
-router.post("/tweets/:id/like", likeTweet);
+router.post("/tweet/:id/like", likeTweet);
 
 // video like/unlike
-router.post("/videos/:id/like", likeVideo);
+router.post("/video/:id/like", likeVideo);
 
 // get all likes of a user
-router.get("/users/:id/likes", likedByUser);
+router.get("/user/:id/likes", likedByUser);
 
 export default router;
